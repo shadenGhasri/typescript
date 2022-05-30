d98dd5909317a06e3fc88de6d8f44aa31e6a7d59
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-05-30 09:45:02 +0430

    commit

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
 export{}
-type Card = {
-    id: number;
-    name: string;
-    image: string;
-    order: number;
-    isFlipped: boolean;
-  }
+const cards = [
+    { id: 1, name: "aurelia", image: aurelia },
+    { id: 2, name: "aurelia", image: aurelia },
+    { id: 3, name: "angular", image: angular },
+    { id: 4, name: "angular", image: angular },
+    { id: 5, name: "ember", image: ember },
+    { id: 6, name: "ember", image: ember },
+    { id: 7, name: "vue", image: vue },
+    { id: 8, name: "vue", image: vue },
+    { id: 9, name: "backbone", image: backbone },
+    { id: 10, name: "backbone", image: backbone },
+    { id: 11, name: "react", image: react },
+    { id: 12, name: "react", image: react },
+  ];
+  
+  export const cardsData = cards.map((card) => ({
+    ...card,
+    order: Math.floor(Math.random() * 12),
+    isFlipped: false,
+  }));
+  
