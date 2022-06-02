4d151472c4c5593c4d1cf1b914d53f8fe81b53a9
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-06-02 20:04:39 +0430

    commit

--- main.ts
+++ main.ts
@@ -32,6 +32,16 @@ const cards = [
   function welcome(user: Account) {
     console.log(user.id)
   }
+
+  type Result = "pass" | "fail"
+ 
+function verify(result: Result) {
+  if (result === "pass") {
+    console.log("Passed")
+  } else {
+    console.log("Failed")
+  }
+}
   
   export const cardsData = cards.map((card) => ({
     ...card,
