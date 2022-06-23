f99e7c5763cef557b62d3f3e7cabac1f34153f02
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-06-23 11:34:23 +0430

    commit

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 export
 const cards = [
     { id: 1, name: "aurelia", image: aurelia }
-    { id: 6, name: "ember", image: ember },
-    { id: 7, name: "vue", image: vue },
-    { id: 9, name: "backbone", image: backbone },
-    { id: 11, name: "react", image: react },
+    { id: 2, name: "ember", image: ember },
+    { id: 3, name: "vue", image: vue },
+    { id: 4, name: "backbone", image: backbone },
+    { id: 5, name: "react", image: react },
   ];
 
   const user = {
