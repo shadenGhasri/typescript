a1709ca04f622e2a8f30ef2371a27777f3bfa8a5
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-06-29 13:19:45 +0430

    commit

--- test.js
+++ test.js
@@ -1,12 +1,21 @@
-function getName(firstName) {
-    return firstName;
-}
-var firstName = "Ali";
-console.log(firstName);
-function barista(name) {
-    console.log('Peppermint Mocha Frappuccino for ' + name);
-}
-var customer = {
-    name: 'Todd'
+// function getName(firstName:string) {
+//     return firstName;
+// }
+// let  firstName="Ali";
+// console.log(firstName);
+// function barista(name) {
+//     console.log('Peppermint Mocha Frappuccino for ' + name);
+//   }
+//   var customer = {
+//     name: 'Todd',
+//   };
+//   barista(customer.name);
+var person = {
+    name: "shaden",
+    hobbies: ["sport", "cooking"]
 };
-barista(customer.name);
+console.log(person.hobbies);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var e = _a[_i];
+    console.log(e.toLocaleUpperCase());
+}
--- test.ts
+++ test.ts
@@ -1,16 +1,31 @@
-function getName(firstName:string) {
-    return firstName;
+// function getName(firstName:string) {
+//     return firstName;
+// }
+
+// let  firstName="Ali";
+
+
+// console.log(firstName);
+
+// function barista(name) {
+//     console.log('Peppermint Mocha Frappuccino for ' + name);
+//   }
+//   var customer = {
+//     name: 'Todd',
+//   };
+//   barista(customer.name);
+
+var person :{
+    name : string ;
+    hobbies : string[]
+} = {
+    name : "shaden",
+    hobbies : ["sport","cooking"]
+}
+console.log(person.hobbies);
+for(let e of person.hobbies){
+    console.log(e.toLocaleUpperCase());
+    
 }
 
-let  firstName="Ali";
 
-
-console.log(firstName);
-
-function barista(name) {
-    console.log('Peppermint Mocha Frappuccino for ' + name);
-  }
-  var customer = {
-    name: 'Todd',
-  };
-  barista(customer.name);
