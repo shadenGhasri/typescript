5419eca07e1a07ef11dc925cf01bf3fbed4e2da3
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-06-29 21:15:49 +0430

    commit

--- test.js
+++ test.js
@@ -41,3 +41,14 @@ var Cars;
 })(Cars || (Cars = {}));
 var car = Cars.VOLVO;
 console.log(car);
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+console.log(combine(2, 5));
--- test.ts
+++ test.ts
@@ -45,3 +45,18 @@ console.log(car);
 
 
 
+
+
+function combine(input1: number | string, input2: number | string) {
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+      result = input1 + input2;
+    } else {
+      result = input1.toString() + input2.toString();
+    }
+    return result;
+  }
+console.log(combine(2,5));
+
+
+
